5f2ad7e8774c2989b01e4249b7ccc2d7f27285c5
faguccio <faga2703@gmail.com>
2021-02-06 23:58:36 +0100

    starting interface of field and room

--- classes/field.hpp
+++ classes/field.hpp
@@ -1,14 +1,25 @@
 /*
 
-Interfaccia
-
-    setup();
-    draw();
-    newStanza();
-    getCampo();
-
-
-
-
 */
 
+#ifndef FIELD_H_INCLUDED
+#define FIELD_H_INCLUDED
+
+#include "room.hpp"
+
+//lista bilinkata di "stanze" 
+struct room_list {
+    Room value;
+    room_list * prec;
+    room_list * next;
+};
+
+class Field {
+    private:
+        room_list game_level;
+
+    public:
+
+};
+
+#endif
---
+++ classes/room.hpp
@@ -0,0 +1,18 @@
+/*
+The level is devided in many room, each one 
+*/
+
+#ifndef ROOM_H_INCLUDED
+#define ROOM_H_INCLUDED
+
+
+
+class Room {
+    private:
+
+
+    public:
+
+};
+
+#endif
